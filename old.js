3a212ad0bcdabcd9e04c19bb1cca8a9ccf0fafad
yuqingzhijie <2711676102@qq.com>
2018-11-05 16:32:24 +0800

    Update old.js

--- old.js
+++ old.js
@@ -1,11 +1,9 @@
-	var colM = [
-        { title: "活动名称", dataIndx: 'actName', width: 200, dataType: "string", align: "center" },
-        { title: "活动类型", dataIndx: 'actTypeName', width: 200, dataType: "string", align: "center"},
-        { title: "支付公司活动编码", dataIndx: 'pcActCode', width: 160, dataType: "string", align: "center"},
-        { title: "对端接入系统", dataIndx: 'systemInfo', width: 160, dataType: "string", align: "center"},
-        { title: "生效时间 <img src=\"img/sort.png\" style=\"cursor:pointer\" name='begin_time'  height=\"12\" alt=\"\"  onclick= sort('begin_time')> ", dataIndx: 'beginTime', width: 160, dataType: "string", align: "center"},
-        { title: "失效时间 <img src=\"img/sort.png\" style=\"cursor:pointer\" name='end_time'  height=\"12\" alt=\"\"  onclick= sort('end_time') >", dataIndx: 'endTime', width: 160, dataType: "string", align: "center"},
-        { title: "创建人", dataIndx: 'createLoginCode', width: 160, dataType: "string", align: "center"},
-        { title: "创建时间 <img src=\"img/sort.png\" style=\"cursor:pointer\" name='create_time'  height=\"12\" alt=\"\"  onclick= sort('create_time')> ", dataIndx: 'createTime', minWidth: 200, dataType: "string", align: "center"},
-        { title: "操作", dataIndx: 'opera', minWidth: 80, dataType: "string", align: "center"}
-    ];
+var colM = [
+    { title: "活动名称", dataIndx: 'actName', width: 200, dataType: "string", align: "center" },
+    { title: "生效时间 <img src=\"img/sort.png\" style=\"cursor:pointer\" name='begin_time'  height=\"12\" alt=\"\" 
+     onclick= sort('begin_time')> ", dataIndx: 'beginTime', width: 160, dataType: "string", align: "center"},
+    { title: "失效时间 <img src=\"img/sort.png\" style=\"cursor:pointer\" name='end_time'  height=\"12\" alt=\"\" 
+     onclick= sort('end_time') >", dataIndx: 'endTime', width: 160, dataType: "string", align: "center"},
+    { title: "创建人", dataIndx: 'createLoginCode', width: 160, dataType: "string", align: "center"},
+    { title: "操作", dataIndx: 'opera', minWidth: 80, dataType: "string", align: "center"}
+];
